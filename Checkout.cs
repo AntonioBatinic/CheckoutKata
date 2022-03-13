d11c2ecd27d98c4b625bec3a2da486eb6d97dad6
AntonioBatinic <a.batinichr@gmail.com>
2022-03-13 18:24:44 +0000

    Created main functionality with basic test suite
    - Added main functionality for Checkout and Offer
    - Added a test suite CheckoutTest for unit-testing basic functionality

---
+++ Checkout.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckoutKata
+{
+    public class Checkout
+    {
+        private Dictionary<char, double> _prices;
+        private Dictionary<char, Offer> _offers;
+        private Dictionary<char, int> _itemCounters;
+
+        // Total sum holder. Can only be set internally
+        public double Total { get; private set; }
+
+        public Checkout()
+        {
+            // Set initial counts of Items to 0
+            _itemCounters = new Dictionary<char, int> { { 'A', 0 }, { 'B', 0 }, { 'C', 0 }, { 'D', 0 } };
+
+            // Set pairs of Items and their Prices
+            _prices = new Dictionary<char, double> { { 'A', 10.0 }, { 'B', 15.0 }, { 'C', 40.0 }, { 'D', 55.0 } };
+
+            // Set Offers for specific items
+            _offers = new Dictionary<char, Offer>
+            {
+                {'B', new Offer { Discount = 5, NumberOfItems = 3}}, // 3 Items for 40
+                {'D', new Offer { Discount = 27.5, NumberOfItems = 2}}, // 25% discount on 2 Items
+            };
+        }
+
+        // Add Item to Cart and calculate the new Total
+        public void AddItem(char item)
+        {
+            // Increase Item count
+            _itemCounters[item]++;
+
+            // Calculate new Total
+            Total += _prices[item];
+
+            // Apply discount if the Item has an offer and if the required number of Items is reached
+            if (HasOffer(item) && RequiredNumber(item))
+            {
+                Total -= _offers[item].Discount;
+            }
+        }
+
+        // Check whether the passed Item has an offer associated
+        private bool HasOffer(char item)
+        {
+            return _offers.ContainsKey(item);
+        }
+
+        // Check whether the required number of Items for discount(Offer) to apply has been added to Cart
+        private bool RequiredNumber(char item)
+        {
+            return _itemCounters[item] == _offers[item].NumberOfItems;
+        }
+    }
+}
---
+++ CheckoutTest.cs
@@ -0,0 +1,72 @@
+﻿using CheckoutKata;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout_Kata.Tests
+{
+    [TestFixture]
+    public class CheckoutTest
+    {
+        [Test]
+        public void AddNoItems_TotalIsZero()
+        {
+            // Arrange
+            var checkout = new Checkout();
+
+            // Act + Assert
+            Assert.AreEqual(0, checkout.Total);
+        }
+
+        [TestCase('A', 10.0)]
+        [TestCase('B', 15.0)]
+        [TestCase('C', 40.0)]
+        [TestCase('D', 55.0)]
+        public void AddOneItem_TotalIsValueItem(char item, double total)
+        {
+            // Arrange
+            var checkout = new Checkout();
+
+            // Act
+            checkout.AddItem(item);
+
+            // Assert
+            Assert.AreEqual(total, checkout.Total);
+        }
+
+        private void AddMultipleItems(string items, double total)
+        {
+            // Arrange
+            var checkout = new Checkout();
+
+            // Act
+            for (int i = 0; i < items.Length; i++)
+            {
+                checkout.AddItem(items[i]);
+            }
+
+            // Assert
+            Assert.AreEqual(total, checkout.Total);
+        }
+
+        [TestCase("AA", 20.0)]
+        [TestCase("AB", 25.0)]
+        [TestCase("ABC", 65.0)]
+        [TestCase("ABCD", 120.0)]
+        public void AddMultipleItems_TotalIsSum(string items, double total)
+        {
+            AddMultipleItems(items, total);
+        }
+
+        [TestCase("AAA", 30.0)]
+        [TestCase("BBB", 40.0)]
+        [TestCase("DD", 82.5)]
+        public void AddMultipleItemsWithOffer_TotalIsSumSubtractDiscount(string items, double total)
+        {
+            AddMultipleItems(items, total);
+        }
+    }
+}
